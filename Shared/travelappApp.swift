07c9c022bb1f81b0beaa2fdfc7fefd4b312012e9
Gosse <thms.gosse@gmail.com>
2021-01-03 17:16:25 +0100

    Initial Commit

---
+++ Shared/travelappApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelappApp.swift
+//  Shared
+//
+//  Created by Thomas Gosse on 03/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct travelappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
